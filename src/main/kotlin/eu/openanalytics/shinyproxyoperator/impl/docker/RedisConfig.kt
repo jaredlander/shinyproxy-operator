c97ed8f8eab833d9ffd440424b009074d38e14a6
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2025-10-20 14:27:54 +0200

    Fix #35726: re-create Redis container in needed

--- src/main/kotlin/eu/openanalytics/shinyproxyoperator/impl/docker/RedisConfig.kt
+++ src/main/kotlin/eu/openanalytics/shinyproxyoperator/impl/docker/RedisConfig.kt
@@ -24,7 +24,6 @@ import eu.openanalytics.shinyproxyoperator.Config
 import eu.openanalytics.shinyproxyoperator.FileManager
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 import org.apache.commons.lang3.RandomStringUtils
 import org.mandas.docker.client.DockerClient
@@ -71,47 +70,51 @@ class RedisConfig(private val dockerClient: DockerClient,
 
     suspend fun reconcile() {
         dockerActions.stopAndRemoveNotRunningContainer(containerName)
-        if (dockerActions.getContainerByName(containerName) == null) {
-            withContext(Dispatchers.IO) {
-                fileManager.createDirectories(dataDir.resolve("data"))
-            }
-            logger.info { "[Redis] Pulling image" }
-            dockerActions.pullImage(redisImage)
+        if (dockerActions.isContainerRunning(containerName, redisImage)) {
+            logger.info { "[Redis] Ok" }
+            return
+        }
+        withContext(Dispatchers.IO) {
+            fileManager.createDirectories(dataDir.resolve("data"))
+        }
+        logger.info { "[Redis] Reconciling" }
+        dockerActions.stopAndRemoveContainer(containerName)
+        logger.info { "[Redis] Pulling image" }
+        dockerActions.pullImage(redisImage)
 
-            withContext(Dispatchers.IO) {
-                fileManager.writeFile(dataDir.resolve("redis.conf"),
-                    """save 60 1
+        withContext(Dispatchers.IO) {
+            fileManager.writeFile(dataDir.resolve("redis.conf"),
+                """save 60 1
                     |appendonly yes
                     |requirepass $redisPassword
                     |""".trimMargin())
-            }
-
-            val hostConfig = HostConfig.builder()
-                .networkMode(DockerOrchestrator.SHARED_NETWORK_NAME)
-                .binds(HostConfig.Bind.builder()
-                    .from(dataDir.resolve("data").toString())
-                    .to("/data")
-                    .build(),
-                    HostConfig.Bind.builder()
-                        .from(dataDir.resolve("redis.conf").toString())
-                        .to("/etc/redis.conf")
-                        .build()
-                )
-                .restartPolicy(HostConfig.RestartPolicy.always())
-                .build()
-
-            val containerConfig = ContainerConfig.builder()
-                .image(redisImage)
-                .hostConfig(hostConfig)
-                .labels(mapOf("app" to "redis"))
-                .cmd(listOf("redis-server", "/etc/redis.conf"))
-                .user(dataDirUid.toString())
-                .build()
-
-            logger.info { "[Redis] Creating new container" }
-            val containerId = dockerClient.createContainer(containerConfig, containerName).id()!!
-            dockerClient.startContainer(containerId)
         }
+
+        val hostConfig = HostConfig.builder()
+            .networkMode(DockerOrchestrator.SHARED_NETWORK_NAME)
+            .binds(HostConfig.Bind.builder()
+                .from(dataDir.resolve("data").toString())
+                .to("/data")
+                .build(),
+                HostConfig.Bind.builder()
+                    .from(dataDir.resolve("redis.conf").toString())
+                    .to("/etc/redis.conf")
+                    .build()
+            )
+            .restartPolicy(HostConfig.RestartPolicy.always())
+            .build()
+
+        val containerConfig = ContainerConfig.builder()
+            .image(redisImage)
+            .hostConfig(hostConfig)
+            .labels(mapOf("app" to "redis"))
+            .cmd(listOf("redis-server", "/etc/redis.conf"))
+            .user(dataDirUid.toString())
+            .build()
+
+        logger.info { "[Redis] Creating new container" }
+        val containerId = dockerClient.createContainer(containerConfig, containerName).id()!!
+        dockerClient.startContainer(containerId)
     }
 
     private fun readPasswordFile(path: String): String? {
